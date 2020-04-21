776066b072b1efdc7adad1d538d1aff04ccd1b5f
Daniel Hinton <djhinton96@outlook.com>
2020-04-22 01:03:18 +1000

    Wrote first 25 questions

---
+++ docs/References.md
@@ -0,0 +1,3 @@
+Questions sourced from episodes of "Millionaire Hot Seat" aired on Channel 9.
+
+All rights to the Who Wants to be a Millionaire game show belong to Sony Pictures Television: https://www.sonypicturestelevision.com/
---
+++ src/question_bank.rb
@@ -0,0 +1,91 @@
+=begin
+Question Structure
+----------------------
+question[0] = Question
+question[1] = Option A
+question[2] = Option B
+question[3] = Option C
+question[4] = Option D
+question[5] = Correct Answer
+
+q1-5 $100
+q6-10 $200
+q11-15 $300
+q16-20 $500
+q21-25 $1,000
+=end
+
+q1 = ["In the 1960s, The Righteous Brothers had a number one hit with 'You've Lost That' What",
+"Lovin' Feeling", "Caring Sensation", "Tender Heart", "Credit Card Again", "A"]
+
+q2 = "In a stadium or arena, the seating area farthest and highest from the stage is known as the what 'section'",
+"Nosebleed", "Headache", "Eyestrain", "Crook Neck", "A"]
+
+q3 = ["In basic mathematics, a whole non-negative number is known as a what number",
+"Natural", "Biodynamic", "Organic", "Gluten-free", "A"]
+
+q4 = ["First airing in 2005 was the long running Australian TV series 'McLeod's' what",
+"Daughter", "Uncles", "Sons", "Neighbours", "A"]
+
+q5 = ["'What's up Doc? is a famous catchphrase of which character'",
+"Elmer Fudd", "Bugs Bunny", "Daffy Duck", "Mr Hyde", "B"]
+
+q6 = ["Born in Philidelphia in 1861, William Wrigley Jr is best known for selling what",
+"Baseball Bats", "Sleeping Bags", "Chewing Gum", "Snakes", "C"]
+
+q7 = ["To be highly proficient at something is to 'have it down to a' what",
+"Political science", "Foreign language", "English major", "Fine art", "D"]
+
+q8 = ["In the classic nursery rhyme 'This Little Piggy', the third little piggy did what",
+"Stayed home", "Had roast beef", "Went to market", "Had none", "B"]
+
+q9 = ["Which of these is a common term that describes a light snowfall?",
+"Sweeping", "Dusting", "Mopping", "Vacuuming", "B"]
+
+q10 = ["The largest portion of something is commonly known as 'the what share'",
+"Shark's", "Pig's", "Tiger's", "Lion's", "D"]
+
+q11 = ["Which of these is the correct spelling for a word meaning a deep shaft, particularly one for drawing water",
+"Bore", "Baw", "Boar", "Boor", "A"]
+
+q12 = ["What colour was the ball used in the first day-night test cricket match",
+"Yellow", "Red", "White", "Pink", "D"]
+
+q13 = ["A colloquial term for a major sporting final is the 'what' dance",
+"Barn", "Bush", "Big", "Belly", "C"]
+
+q14 = ["Which of these is the name of a city in Ireland?",
+"Cork", "Flint", "Hemp", "Felt", "A"]
+
+q15 = ["Which of these words is both someone who rides a horse and a verb meaning 'to manoeuvre for advantage'",
+"Cowboy", "Mountie", "Jockey", "Equestrian", "C"]
+
+q16 = ["Which of these is a type of gemstone",
+"Beryl", "Ethyl", "Myrtle", "Phyllis", "A"]
+
+q17 = ["Metal halide is the name for a type of what, commonly found in public spaces",
+"Garbage Bin", "Lamp", "Park Bench", "Fence", "B"]
+
+q18 = ["Which of these sports has a marked area known as the 'key'",
+"Tennis", "Boxing", "Snooker", "Basketball", "D"]
+
+q19 = ["Which of these is a term meaning to force or trick a person into something",
+"Wuhan", "Guangdong", "Shanghai", "Nanjing", "C"]
+
+q20 = ["Which of these most closely decribes an 'anomaly'",
+"Calculated risk", "Deviation from the norm", "Binding agreement", "Type of monopoly", "B"]
+
+q21 = ["Which of these is a 'proper fraction'",
+"3/3", "3/5", "5/5", "5/3", "B"]
+
+q22 = ["'Thank U, Next' was an Australian number one hit single in 2018 for which singer",
+"Cardi B", "Ariana Grande", "Mariah Carey", "Halsey", "B"]
+
+q23 = ["A free national mobile phone recycling scheme in Australia is known as 'Mobile' what",
+"Monitor", "Mountain", "Meltdown", "Muster", "D"]
+
+q24 = ["In golf, if someone scores a bogey on a par five hole, how many shots did they hit",
+"Three", "Four", "Five", "Six", "D"]
+
+q25 = ["The village and historic site of Port Arthur is located on which section of Tasmania's coastline",
+"North-east", "North-west", "South-east", "South-west", "C"]
