50d0b3c6d9b8a8c2f25406865c110005bfe0abee
Daniel Hinton <djhinton96@outlook.com>
2020-04-26 15:36:09 +1000

    Test outline created

--- tests/unittests.rb
+++ tests/unittests.rb
@@ -1,6 +1,7 @@
 # Unit testing the functions in src/functions.rb
 
 require 'test/unit'
+
 require 'colorize'
 
 require_relative '../src/functions.rb'
@@ -12,8 +13,4 @@ class FunctionTest < Test::Unit::TestCase
   include Functions
   include QuestionBank
 
-  samplequestion = ["In the 1960s, The Righteous Brothers had a number one hit
-  with 'You've Lost That' What", "Lovin' Feeling", "Caring Sensation",
-  "Tender Heart", "Credit Card Again", "A", 84, 7, 6, 3]
-  
 end
